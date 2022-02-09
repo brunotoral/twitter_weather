15999e2df4c4c0dba670be2e02d46a4f7cb75a68
Bruno Toral <brunotoral89@gmail.com>
2022-02-09 10:05:22 -0300

    Fix RenderTweetService spec setup

--- spec/services/render_tweet_service_spec.rb
+++ spec/services/render_tweet_service_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RenderTweetService do
+  include ActiveSupport::Testing::TimeHelpers
+
   let(:temperatures) { { "07/02" => 27.8, "08/02" => 27.3, "09/02" => 27.3, "10/02" => 27.4, "11/02" => 27.9 } }
 
   before do
@@ -26,9 +28,11 @@ RSpec.describe RenderTweetService do
 
   describe '.composed_message' do
     it 'returns forecast informations' do
-      render = described_class.new(temps: file_data('forecast'), current: file_data('weather'))
+       travel_to Date.new(2022,02,06) do
+        render = described_class.new(temps: temperatures, current: file_data('weather'))
 
-      expect(render.composed_message).to eq '30.36°C e nublado em Natal em 08/02. Média para os próximos dias: 27.8°C em 07/02, 27.3°C em 08/02, 27.3°C em 09/02, 27.4°C em 10/02 e 27.9°C em 11/02.'
+        expect(render.composed_message).to eq '30.36°C e nublado em Natal em 06/02. Média para os próximos dias: 27.8°C em 07/02, 27.3°C em 08/02, 27.3°C em 09/02, 27.4°C em 10/02 e 27.9°C em 11/02.'
+      end
     end
   end
 end
